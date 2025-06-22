23fa8aad04d5b20dfc00cefd2bae4e09d076395a
Shreeshanth shetty <shreeshanthshetty@gmail.com>
2025-06-22 18:39:53 +0530

    feat(frontend): implement login page with animated SVG heart (#19)
    * feat(frontend): implement login page with animated SVG heart
    
    Added login page UI as per Figma with SVG heart animation using stroke-dashoffset animation.
    
    Closes #9

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,7 +2,8 @@ import { useState, useEffect } from "react";
 import "./Login.css";
 import Button from "../../components/button/Button";
 import { checkLoginStatus } from "../../utils/checkLoginStatus";
-
+import HeartAnimation from "./HeartAnimation";
+import backgroundImage from  '../../assets/login/bg-login.png'
 const Login = ({ onLogin }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -29,19 +30,26 @@ const Login = ({ onLogin }) => {
   if (loggedIn) {
     window.location.href = "/";
   }
-
   return (
-    <div className="login-container">
-      <span>Sign in to YDO</span>
-      <Button
-        onClick={handleGoogleLogin}
-        disabled={isLoading}
-        className="primary"
-      >
-        {isLoading ? "loading..." : "Sign in with Google"}
-      </Button>
-      {error && <p className="error-message">{error}</p>}
+    <>
+    <div className="container" style={{backgroundImage:`url(${backgroundImage})`}}>
+      <div className="login-container">
+        <HeartAnimation />
+        <div className="login-card">
+          <div className="Title">YOU DESERVE ONE</div>
+          <div className="google-button" onClick={handleGoogleLogin}>
+            <div className="text">LOGIN WITH</div>
+           <div className="wrapper"><div className="google-icon"></div></div> 
+          </div>   
+          <p className="confidential-text">
+            All your information is confidential
+            <br />
+            Even the developers can’t access your data.read{" "}how
+          </p>
+        </div>
+      </div>
     </div>
+    </>
   );
 };
 
