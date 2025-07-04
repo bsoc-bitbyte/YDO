703cf9d1c6bf1f24497aaf6f70d3d7812813fd00
Zapper9982 <144029909+Zapper9982@users.noreply.github.com>
2025-07-04 14:05:13 +0530

    Revert "feat(frontend): add reusable animation component"

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -2,18 +2,23 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
 import backgroundImage from "../../assets/Landingpage/bg.jpg";
-import YDOAnimation from "../../components/Animation/Animation";
+import arrowImage from "../../assets/Landingpage/arrowImage.svg";
 import line from "../../assets/Landingpage/Line.svg";
 
 function Landingpage(){
     const navigate = useNavigate();
 
-    return (        
-        <div className="landing_page" style={{ backgroundImage: `url(${backgroundImage})` }}>            
+    return (
+        <div className="landing_page" style={{ backgroundImage: `url(${backgroundImage})` }}>
             <div className="landing_page_content">
-                <div className="animation">
-                    <YDOAnimation />
-                </div>                
+                <div id="ydo">
+                    <div className="logo_y">Y</div>
+                    <div className="logo_d">D</div>
+                    <div className="image__wrapper">
+                        <img src={arrowImage} alt="arrow" className="arrow" />
+                    </div>
+                    <div className="logo_o">O</div>
+                </div>
                 <p>
                     <div className="line">
                         <img src={line} alt="line" className="Line" />
