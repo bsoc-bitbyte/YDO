b4d9a50ed984061d9864d73a9c23904bf24d989e
SHREESHANTH99 <shreeshanthshetty@gmail.com>
2025-07-10 10:47:10 +0530

    feat(frontend): added Protection to Frontend Made changes according to requirement. Closes #25

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,32 @@ import Login from "./pages/Login/Login";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import Callback from "./pages/Callback/Callback";
-import NotFound from './pages/404/404';
+import NotFound from "./pages/404/404";
 import PrivateRoute from "./components/ProtectedRoutes/ProtectedRoutes.jsx";
 import MainLayout from "./layouts/MainLayout.jsx";
+import Preloader from "./pages/Preloader/Preloader";
 import ProfilePage from "./pages/Profile/ProfilePage.jsx";
-
 
 function App() {
   return (
     <Routes>
-      <Route element={<MainLayout/>}>
-      <Route path="/" element={<Landingpage />} />
-      <Route path="/home" element={<PrivateRoute><Home/></PrivateRoute>}/>
-      <Route path="/login" element={<Login />} />
- <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
-      <Route path="/callback" element={<Callback/>}/>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Landingpage />} />
+        <Route
+          path="/home"
+          element={<PrivateRoute> <Home /></PrivateRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<PrivateRoute><ProfilePage /></PrivateRoute>}/>
+        <Route path="/callback" element={<Callback />} />
       </Route>
+      <Route path="/preloader" element={<Preloader />} />
+      
 
-      <Route path="/callback" element={<Callback />} />
-      <Route path="/profile" element={<ProfilePage />} />
+
       <Route path="*" element={<NotFound />} />
-     </Routes>
-  )
+    </Routes>
+  );
 }
-
 export default App;
