ed2154273337bf50562e0f786971421a29b9833a
Arunit Chakraborty <arunitchakraborty2006@gmail.com>
2025-07-21 23:19:57 +0530

    Fix #29: added infinite carousel with lazy loading and skeleton cards (#37)
    * feat: added infinite carousel and styled home page
    
    * feat(frontend): made it resposive for tablets
    
    * feat(frontend): finished up carousel with backend interop
    
    -added backend integration and handled the loading asynchronously
    -added lazy loading to images to improve performence
    -added skeleton cards to improve ux
    
    Closes #29
    
    * refactor: removed uneccesary stuff
    
    * fix: resolved no default export in fetchProfiles.js

---
+++ src/components/infinite-carousel/EmblaCarousel.jsx
@@ -0,0 +1,206 @@
+import React, { useEffect, useState, useCallback } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import nextArrow from "../../assets/carousel/nextArrow.svg";
+import prevArrow from "../../assets/carousel/prevArrow.svg";
+import fetchUsers from "../../utils/fetchProfiles";
+import "./EmblaCarousel.css";
+
+const ProfileCard = ({
+  profile,
+  index,
+  selectedSlide,
+  isVisible,
+  fadeComplete,
+}) => {
+  return (
+    <>
+      <div className="embla__slide" key={profile.id}>
+        <div
+          className={`card__container ${
+            index === selectedSlide ? "selected" : ""
+          } ${!fadeComplete ? (isVisible ? "fade-in" : "fade-out") : ""}`}
+        >
+          <div className="card__img-wrapper">
+            <img
+              className="card__img"
+              src={profile.image}
+              alt="pic"
+              loading="lazy"
+            />
+          </div>
+          <div className="card__data">
+            <div className="card__header">
+              <h3 className="card__data--heading">
+                {profile.name} ({profile.age})
+              </h3>
+              <p className="card__data--subheading">
+                {profile.discipline} - {profile.year}
+              </p>
+            </div>
+            <div className="card__tags">
+              {profile.tags.map((tag, idx) => (
+                <span className="tag" key={idx}>
+                  {tag}
+                </span>
+              ))}
+            </div>
+            <div className="card__description">
+              <p>{profile.description}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const SkeletonCard = ({ index, selectedSlide }) => {
+  return (
+    <>
+      <div className="embla__slide">
+        <div
+          className={`card__container skeleton-card ${
+            index === selectedSlide ? "selected" : ""
+          }`}
+        >
+          <div className="card__img-wrapper--skeleton skeleton"></div>
+          <div className="card__data">
+            <div className="card__header--skeleton">
+              <div className="card__data--heading--skeleton skeleton"></div>
+              <div className="card__data--subheading--skeleton skeleton"></div>
+            </div>
+            <div className="card__tags--skeleton">
+              {Array.from({ length: 4 }).map((_, idx) => (
+                <div className="tag--skeleton skeleton" key={idx}></div>
+              ))}
+            </div>
+            <div className="card__description--skeleton skeleton"></div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+const EmblaCarousel = () => {
+  const [emblaRef, emblaApi] = useEmblaCarousel({
+    loop: true,
+    align: "center",
+  });
+
+  //Backend integration
+
+  const [profiles, setProfiles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [fadeComplete, setFadeComplete] = useState(false);
+  const [contentVisible, setContentVisible] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const loadProfiles = async () => {
+      const { data, error } = await fetchUsers(controller.signal);
+      if (error) {
+        setError(error);
+      } else {
+        setProfiles(data);
+      }
+      setLoading(false);
+      setTimeout(() => setContentVisible(true), 100);
+    };
+
+    loadProfiles();
+
+    return () => controller.abort();
+  }, []);
+
+  useEffect(() => {
+    if (contentVisible && !fadeComplete) {
+      const timer = setTimeout(() => {
+        setFadeComplete(true);
+      }, 600);
+
+      return () => clearTimeout(timer);
+    }
+  }, [contentVisible, fadeComplete]);
+
+  //state control for the carousel
+
+  const [selectedSlide, setSelectedSlide] = useState(0);
+
+  const onSelect = useCallback(() => {
+    if (!emblaApi) return;
+    setSelectedSlide(emblaApi.selectedScrollSnap());
+  }, [emblaApi, setSelectedSlide]);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onSelect();
+    emblaApi.on("select", onSelect);
+    emblaApi.on("reInit", onSelect);
+
+    return () => {
+      emblaApi.off("select", onSelect);
+      emblaApi.off("reInit", onSelect);
+    };
+  }, [emblaApi, onSelect]);
+
+  const scrollNext = () => emblaApi && emblaApi.scrollNext();
+  const scrollPrev = () => emblaApi && emblaApi.scrollPrev();
+
+  return (
+    <>
+      <div className="embla">
+        {loading && (
+          <>
+            <div className="embla__viewport" ref={emblaRef}>
+              <div className="embla__container">
+                {Array.from({ length: 4 }).map((_, index) => (
+                  <SkeletonCard index={index} selectedSlide={selectedSlide} />
+                ))}
+              </div>
+            </div>
+            <div className="embla__next" onClick={scrollNext}>
+              <img src={nextArrow} alt="next" />
+            </div>
+            <div className="embla__prev" onClick={scrollPrev}>
+              <img src={prevArrow} alt="prev" />
+            </div>
+          </>
+        )}
+        {error && <div className="carousel__error">Error: {error}</div>}
+        {!loading && !error && (
+          <>
+            <div className="embla__viewport" ref={emblaRef}>
+              <div className="embla__container">
+                {profiles?.length > 0 ? (
+                  profiles.map((profile, index) => (
+                    <ProfileCard
+                      profile={profile}
+                      index={index}
+                      selectedSlide={selectedSlide}
+                      isVisible={contentVisible}
+                      fadeComplete={fadeComplete}
+                    />
+                  ))
+                ) : (
+                  <div>No profiles found</div>
+                )}
+              </div>
+            </div>
+            <div className="embla__next" onClick={scrollNext}>
+              <img src={nextArrow} alt="next" />
+            </div>
+            <div className="embla__prev" onClick={scrollPrev}>
+              <img src={prevArrow} alt="prev" />
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default EmblaCarousel;
