d2ee928537322a4486cde6c43b49b76d1144c159
Sampath-1984 <b.jnanasampath10cmplus@gmail.com>
2025-08-02 13:28:54 +0530

    feat(backend-db): sql schema containing tables and relationships
    This commit makes minor changes to the schema.sql as requested by the reviewer.
    
    Closes #39

--- backend/db/schema.sql
+++ backend/db/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE user_info (
 	id BIGSERIAL NOT NULL PRIMARY KEY,
 	name VARCHAR(50) NOT NULL,
-	age SMALLINT NOT NULL CHECK(age > 0),
+	dob DATE NOT NULL,
 	year SMALLINT NOT NULL CHECK(year IN (1,2,3,4)),
 	branch VARCHAR(50) NOT NULL CHECK(branch IN ('CSE','ECE','MECH','SM','BDES')),
 	bio VARCHAR(500),
@@ -21,12 +21,12 @@ CREATE TABLE tags_user_table (
 
 CREATE TABLE choices_for_chooser (
 	chooser_id BIGINT NOT NULL REFERENCES user_info(id),
-	chosen_id BIGINT REFERENCES user_info(id),
+	chosen_id VARCHAR(100),
 	UNIQUE (chooser_id)
 );
 
 CREATE TABLE choices_for_chosen (
 	chosen_id BIGINT NOT NULL REFERENCES user_info(id),
-	chooser_id BIGINT REFERENCES user_info(id),
+	chooser_id VARCHAR(100),
 	UNIQUE (chosen_id)
 );
